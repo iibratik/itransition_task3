a80505ffe91c2db8413edd7624942d9262e2cf6e
iibratik <iibragimov2417@gmail.com>
2026-01-31 22:18:14 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -8,27 +8,33 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 app.get('/iibragimov2417_gmail_com', (req, res) => {
-  const x: number = Number(req.query.x);
-  const y: number = Number(req.query.y);
-  const result = lcm(x,y)
+  const x = Number(req.query.x);
+  const y = Number(req.query.y);
+
+  const result = lcm(x, y);
+
+  // Устанавливаем тип ответа "обычный текст"
+  res.setHeader('Content-Type', 'text/plain');
+
   if (isNaN(result)) {
-    return res.send('NaN');
+    res.send('NaN');
   } else {
-    res.send(result)
+    // Важно: отправляем как строку, чтобы Express не принял это за статус-код
+    res.send(result.toString());
   }
 });
 function gcd(x:number,y:number){
-  while (y !== 0) {
+while (y !== 0) {
     x %= y;
     [x, y] = [y, x];
   }
   return x;
 }
+function isNatural(n: unknown): n is number {
+  return typeof n === 'number' && Number.isInteger(n) && n > 0;
+}
 function lcm(x:number, y:number):number{
-  function isNatural(n:unknown): n is number{
-   return typeof n === 'number' && Number.isInteger(n) && n > 0;
-  }
-  if (!isNatural(x) || !isNatural(y)) {
+if (!isNatural(x) || !isNatural(y)) {
     return NaN;
   }
   return (x * y) / gcd(x, y);
